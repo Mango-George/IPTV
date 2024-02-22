a9090fd159ce3a13aa9db9e4b2db2ea7f237c430
Lx.Sun <lx.sun@igg.com>
2024-02-22 18:05:20 +0800

    增加工程

---
+++ ExcelLoader/ExcelLoader/Form1.cs
@@ -0,0 +1,21 @@
+﻿using ExcelLoader.Scripts;
+using System;
+using System.Windows.Forms;
+
+namespace ExcelLoader
+{
+    public partial class Form1 : Form
+    {
+        private ExcelHandler excelHandler;
+        public Form1()
+        {
+            excelHandler = new ExcelHandler();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            excelHandler.Export();
+        }
+    }
+}
---
+++ ExcelLoader/ExcelLoader/Scripts/ExcelHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelLoader.Scripts
+{
+    public class ExcelHandler
+    {
+        public void Export()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("Datas");
+            FileInfo[] fileInfos = directoryInfo.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
+            Console.Write(fileInfos.Length);
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                Console.WriteLine(fileInfo.FullName);
+            }
+        }
+    }
+}
