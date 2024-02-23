75052ff49269a06b9358ed498e3320e29b54b6c1
Lx.Sun <lx.sun@igg.com>
2024-02-23 14:46:21 +0800

    修改工具

--- ExcelLoader/ExcelLoader/Scripts/ExcelHandler.cs
+++ ExcelLoader/ExcelLoader/Scripts/ExcelHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.IO;
 
 namespace ExcelLoader.Scripts
@@ -7,12 +8,34 @@ namespace ExcelLoader.Scripts
     {
         public void Export()
         {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             DirectoryInfo directoryInfo = new DirectoryInfo("Datas");
             FileInfo[] fileInfos = directoryInfo.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly);
             Console.Write(fileInfos.Length);
-            foreach (FileInfo fileInfo in fileInfos)
+            // foreach (FileInfo fileInfo in fileInfos)
+            FileInfo fileInfo = fileInfos[0];
             {
                 Console.WriteLine(fileInfo.FullName);
+                using (FileStream stream = new FileStream(fileInfo.FullName, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    try
+                    {
+                        ExcelPackage excelPackage = new ExcelPackage(stream);
+                        ExcelWorkbook workbook = excelPackage.Workbook;
+                        int sheetCount = workbook.Worksheets.Count;
+                        for (int i = 0; i < sheetCount; i++)
+                        {
+                            ExcelWorksheet worksheet = workbook.Worksheets[i];
+                            int row = worksheet.Dimension.End.Row;
+                            int column = worksheet.Dimension.End.Column;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
             }
         }
     }
